8ba84a98dbe144c99925cd01c47383d35c2e413b
Neillon Cesar <neilloncesar13@gmail.com>
2019-03-11 15:38:49 -0300

    Resolving conflicts

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
-const app = require('./server');
+require("dotenv").config();
 
+const app = require('./server');
 const port = process.env.PORT || 3000;
 
-<<<<<<< HEAD
-app.use(process.env.NODE_ENV === "dev" ?
-  morgan("dev") :
-  morgan("tiny"));
-
-app.get('/', (req, res) => {
-  return res.json({
-    message: 'ok'
-  });
-});
-
-app.use(require('./middlewares/cors'))
-
-=======
->>>>>>> feature/login
 app.listen(3000, () => {
   console.log(`Server is running on port ${port}`);
 });
--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require('body-parser');
@@ -10,6 +8,11 @@ app.use(bodyParser.json());
 
 app.use(process.env.NODE_ENV === "dev" ? morgan("dev") : morgan("tiny"));
 
+app.use(process.env.NODE_ENV === "dev" ?
+    morgan("dev") :
+    morgan("tiny"));
+
+app.use(require('./middlewares/cors'))
 app.use('/api', require('./routes/login'));
 
 module.exports = app;
